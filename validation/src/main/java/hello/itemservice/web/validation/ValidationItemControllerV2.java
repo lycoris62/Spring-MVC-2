abcad0ce15dd4274d51ecd4b156d6ce446db51a2
Lycoris62 <wodbs7758@naver.com>
2023-03-18 14:57:47 +0900

    섹션 4. 검증1 - Validation : 오류 코드와 메시지 처리3

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -159,6 +159,7 @@ public class ValidationItemControllerV2 {
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
+            // new String[]{"required.item.itemName", "required"}
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
