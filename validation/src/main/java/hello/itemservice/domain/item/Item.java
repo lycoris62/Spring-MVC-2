066411db2fcae17b7a13afcae7508cac896d8fc8
Lycoris62 <wodbs7758@naver.com>
2023-03-24 15:46:29 +0900

    섹션 5. 검증2 - BeanValidation : Bean Validation - groups

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
 public class Item {
 
-    @NotNull // 수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(message = "상품 이름은 필수입니다.")
+    @NotBlank(message = "상품 이름은 필수입니다.", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(message = "가격은 필수입니다.")
+    @NotNull(message = "가격은 필수입니다.", groups = {SaveCheck.class, UpdateCheck.class})
     @Range(min = 1000, max = 1000000)
     private Integer price;
 
-    @NotNull
-//    @Max(9999) // 수정 요구사항 추가
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
